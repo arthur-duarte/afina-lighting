9406f3bb6643c9b2d9c7a86aeeae2cdff19f6905
arthur-duarte <arthursalesduarte@gmail.com>
2026-07-30 11:51:47 -0300

    fix: add canvas fallback in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['konva', 'react-konva'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'canvas'];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
